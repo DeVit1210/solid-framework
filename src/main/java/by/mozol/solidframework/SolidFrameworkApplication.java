2aca1cff4d4721df9fd112b12c0da91969e2eb51
Mozol Denis <mozolden7@gmail.com>
2025-11-02 18:24:43 +0300

    Initial commit

---
+++ src/main/java/by/mozol/solidframework/SolidFrameworkApplication.java
@@ -0,0 +1,13 @@
+package by.mozol.solidframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidFrameworkApplication.class, args);
+	}
+
+}
